c86dc2448f828409456c2c4f8ffb195e9a4087a9
Tom Johansson <tom.johansson123@gmail.com>
2022-11-23 16:41:40 +0100

    [issue] jQuery toggle
    jquery toggle not working when importing data from different folder

--- js/script.js
+++ js/script.js
@@ -1,117 +1,127 @@
+// events = [
+//   {
+//     name: "Air force jam",
+//     date: "1 jan 2020",
+//     short_desc: "this party is for short shoes unless you have tall ones.",
+//     category: "sneakers",
+//     link: "details.html",
+//     image: "https://picsum.photos/200",
+//   },
+//   {
+//     name: "blockfeet party",
+//     date: "1 jan 2020",
+//     short_desc: "this party is for crypto shoes,bring your blockfeet",
+//     category: "sneakers",
+//     link: "details.html",
+//     image: "https://picsum.photos/200",
+//   },
+//   {
+//     name: "Ground force jammed",
+//     date: "1 jan 2020",
+//     short_desc: "if youre not wearing shoes you cant come",
+//     category: "shoeless",
+//     link: "details.html",
+//     image: "https://picsum.photos/200",
+//   },
+//   {
+//     name: "vs code party",
+//     date: "1 jan 2020",
+//     short_desc: " this event is for all who dont know what shoes are",
+//     category: "code",
+//     link: "details.html",
+//     image: "https://picsum.photos/200",
+//   },
+//   {
+//     name: "shoelace party",
+//     date: "1 jan 2050",
+//     short_desc: "this party is for all who have shoelaces",
+//     category: "shoelaces",
+//     link: "details.html",
+//     image: "https://picsum.photos/200",
+//   },
+//   {
+//     name: "sole party",
+//     date: "1 jan 2040",
+//     short_desc: "this party is for all who have soles",
+//     category: "soles",
+//     link: "details.html",
+//     image:
+//       "https://www.kia.com/content/dam/kwcms/kme/global/en/assets/vehicles/e-Soul/specfication/kia-e-soul-my19-slide-list-01-w.png",
+//   },
+//   {
+//     name: "javascript party",
+//     date: "1 jan 2030",
+//     short_desc: "this party is for all who have javascript",
+//     category: "javascript",
+//     link: "details.html",
+//     image: "https://picsum.photos/200",
+//   },
+//   {
+//     name: "react party",
+//     date: "1 jan 2020",
+//     short_desc: "this party is for all who have react",
+//     category: "react",
+//     link: "details.html",
+//     image: "https://picsum.photos/200",
+//   },
+//   {
+//     name: "node party",
+//     date: "1 jan 2020",
+//     short_desc: "this party is for all who have node",
+//     category: "node",
+//     link: "details.html",
+//     image: "https://picsum.photos/200",
+//   },
+//   {
+//     name: "express party",
+//     date: "1 jan 2020",
+//     short_desc: "this party is for all who have express",
+//     category: "express",
+//     link: "details.html",
+//     image: "https://picsum.photos/200",
+//   },
+//   {
+//     name: "mongo party",
+//     date: "1 jan 2020",
+//     short_desc: "this party is for all who have mongo",
+//     category: "mongo",
+//     link: "details.html",
+//     image: "https://picsum.photos/200",
+//   },
+//   {
+//     name: "mongoose party",
+//     date: "1 jan 2020",
+//     short_desc: "this party is for all who have mongoose",
+//     category: "mongoose",
+//     link: "details.html",
+//     image: "https://picsum.photos/200",
+//   },
+//   {
+//     name: "html party",
+//     date: "1 jan 2020",
+//     short_desc: "this party is for all who have html",
+//     category: "html",
+//     link: "details.html",
+//     image: "https://picsum.photos/200",
+//   },
+// ];
+
 $(document).ready(function () {
-  console.log("ready!");
+  // addArticles();
 
-  const events = [
-    {
-      name: "Air force jam",
-      date: "1 jan 2020",
-      short_desc: "this party is for short shoes unless you have tall ones.",
-      category: "sneakers",
-      link: "details.html",
-      image: "https://picsum.photos/200",
-    },
-    {
-      name: "blockfeet party",
-      date: "1 jan 2020",
-      short_desc: "this party is for crypto shoes,bring your blockfeet",
-      category: "sneakers",
-      link: "details.html",
-      image: "https://picsum.photos/200",
-    },
-    {
-      name: "Ground force jammed",
-      date: "1 jan 2020",
-      short_desc: "if youre not wearing shoes you cant come",
-      category: "shoeless",
-      link: "details.html",
-      image: "https://picsum.photos/200",
-    },
-    {
-      name: "vs code party",
-      date: "1 jan 2020",
-      short_desc: " this event is for all who dont know what shoes are",
-      category: "code",
-      link: "details.html",
-      image: "https://picsum.photos/200",
-    },
-    {
-      name: "shoelace party",
-      date: "1 jan 2050",
-      short_desc: "this party is for all who have shoelaces",
-      category: "shoelaces",
-      link: "details.html",
-      image: "https://picsum.photos/200",
-    },
-    {
-      name: "sole party",
-      date: "1 jan 2040",
-      short_desc: "this party is for all who have soles",
-      category: "soles",
-      link: "details.html",
-      image:
-        "https://www.kia.com/content/dam/kwcms/kme/global/en/assets/vehicles/e-Soul/specfication/kia-e-soul-my19-slide-list-01-w.png",
-    },
-    {
-      name: "javascript party",
-      date: "1 jan 2030",
-      short_desc: "this party is for all who have javascript",
-      category: "javascript",
-      link: "details.html",
-      image: "https://picsum.photos/200",
-    },
-    {
-      name: "react party",
-      date: "1 jan 2020",
-      short_desc: "this party is for all who have react",
-      category: "react",
-      link: "details.html",
-      image: "https://picsum.photos/200",
-    },
-    {
-      name: "node party",
-      date: "1 jan 2020",
-      short_desc: "this party is for all who have node",
-      category: "node",
-      link: "details.html",
-      image: "https://picsum.photos/200",
-    },
-    {
-      name: "express party",
-      date: "1 jan 2020",
-      short_desc: "this party is for all who have express",
-      category: "express",
-      link: "details.html",
-      image: "https://picsum.photos/200",
-    },
-    {
-      name: "mongo party",
-      date: "1 jan 2020",
-      short_desc: "this party is for all who have mongo",
-      category: "mongo",
-      link: "details.html",
-      image: "https://picsum.photos/200",
-    },
-    {
-      name: "mongoose party",
-      date: "1 jan 2020",
-      short_desc: "this party is for all who have mongoose",
-      category: "mongoose",
-      link: "details.html",
-      image: "https://picsum.photos/200",
-    },
-    {
-      name: "html party",
-      date: "1 jan 2020",
-      short_desc: "this party is for all who have html",
-      category: "html",
-      link: "details.html",
-      image: "https://picsum.photos/200",
-    },
-  ];
+  loadData();
 
-  function addArticles() {
+  async function loadData() {
+    const response = await fetch("../data/data.json");
+    const data = await response.json();
+    //parse the json data
+    console.log(data.events);
+    addArticles(data.events);
+    hideText();
+  }
+
+  function addArticles(events) {
     for (let i = 0; i < events.length; i++) {
-      const description = events[i].short_desc.substring(0, 30);
       const table = $(`
     <article id="${["article" + i]}">
         <img src="${events[i].image}" alt="image">
@@ -126,10 +136,12 @@ $(document).ready(function () {
                 </tr>
                 <tr>
                     <th>Description</th>
-                    <td><button class="toggle1" id="${[i]}">More</button></td>
-                    <td class="hidden" id="p-${[i]}">${
+                    <td><button class="toggle1" id="${[
+                      i,
+                    ]}">Show text</button></td>
+                    <div class="hidden" id="p-${[i]}"><h3>📖</h3><p>${
         events[i].short_desc
-      }</td>
+      }</p></div>
                 </tr>
                 <tr>
                     <th>Category</th>
@@ -146,61 +158,58 @@ $(document).ready(function () {
     }
   }
 
-  addArticles();
-
-  //this will cause the articles to be change background color when clicked
-
   //this is to add click effect to the table
   $(".toggle1").click(function () {
     buttonId = this.id;
     $(this).toggleClass("clicked");
     if ($(this).hasClass("clicked")) {
       $("#p-" + buttonId).show();
-      $(this).text("less");
+      $(this).text("hide text");
     } else {
       $("#p-" + buttonId).hide();
       //   $("#p-" + buttonId).text();
-      $(this).text("More");
+      $(this).text("Show text");
     }
   });
-
-  // $(".toggle").click(function () {
-  //   //console log to see if the button is clicked
-  //   console.log(this.id);
-  //   //replace the text with the full description
-
-  //   //
-  // });
-  //   $(".toggle1").click(function () {
-  //     buttonId = this.id;
-  //     //this code will show the full description from the td with same id as the button
-  //     $("#p-" + buttonId).show();
-  //     //this code will hide the button
-
-  //     console.log(buttonId);
-  //   });
-
-  //this function will add
 
   //this function will hide the text if it has classname of hidden
   function hideText() {
     $(".hidden").hide();
   }
-  hideText();
-});
-$("#thank-you").hide();
-$("#create-event").click(function () {
-  console.log("clicked");
+  //this function will show the text if it has classname of hidden
+  $("#thank-you").hide();
 
-  //this will hide the id event-form-container and replace it with a thank you message
-  $("#create-event-form").hide(1000);
-  $("#thank-you").show(1000);
+  $("#create-event").click(function () {
+    //this will hide the id event-form-container and replace it with a thank you message
+    $("#create-event-form").hide(1000);
+    $("#thank-you").show(1000);
 
-  // this will uppdate page after 5 seconds
-  setTimeout(function () {
-    location.reload();
-  }, 5000);
-  return false;
-});
+    // this will uppdate page after 5 seconds
+    setTimeout(function () {
+      location.reload();
+    }, 5000);
+    return false;
+  }); //end of create-event click function
 
-//this will code will run when the button is clicked
+  $("#send-message-btn").click(function () {
+    // append the message to the body
+    const message = $("#message").val();
+    const name = $("#name").val();
+    const email = $("#email").val();
+    const phone = $("#phone").val();
+    const messageContainer = $(`
+    <div class="message-container">
+    <h3>Message from ${name}</h3>
+    <p>${message}</p>
+    <p>${email}</p>
+    <p>${phone}</p>
+    </div>
+    `);
+    // replace the document with the message
+    $("body").replaceWith(messageContainer);
+    //keep document for 5 seconds
+    setTimeout(function () {
+      location.reload();
+    }, 5000);
+  }); //end of send-message click function
+}); //end of document ready
